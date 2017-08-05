f2b211dc353c555fb21c51c78f98266fd84f6549
Teamzone <teamzone@aboutagile.com>
2017-08-05 19:33:56 +0000

    First failing test

---
+++ stringCalcTests.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var assert = require('assert'); 
+
+// Potential to divide this up into Testcase Class per TestFixture.  It could get big nd unwieldy
+
+describe("String Calculator Tests", function () {
+    
+    //1. Module Setup
+    
+    //Shared Fixture for all tests
+    before(function () {
+
+    });
+    
+    // Fresh Fixture per test.  
+    beforeEach(function () {
+
+    });
+    
+    // 2. Module Exercise
+    
+    it("'0' + '0' returns '0'", function (done) {
+        // 1. Setup
+        
+        // 2. Exercise
+        
+        // 3. Verify
+        assert(false, 'Nothing defined yet');
+        
+        // 4. Cleanup & Finish
+        done();
+    });
+    
+
+});
