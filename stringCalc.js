7a7600a5b25d423651b6dd35932d59f6aebaaf0a
Teamzone <teamzone@aboutagile.com>
2017-08-05 19:53:47 +0000

    A simple yet not fit for purpose implementation

--- stringCalc.js
+++ stringCalc.js
@@ -2,7 +2,12 @@ function StringCalc() {
 }
 
 StringCalc.prototype.Add = function (operand1, operand2) {
-    return '';
+    var result;
+    var number1 = parseInt(operand1, 10);
+    var number2 = parseInt(operand2, 10);
+    result = number1 + number2;
+    
+    return result.toString();
 };
     
 module.exports = StringCalc;
