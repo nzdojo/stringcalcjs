2ce6209edfc4f2ce8e717aa8b29fa4b2694336fb
Teamzone <teamzone@aboutagile.com>
2017-08-05 20:04:00 +0000

    Refactor to remove duplication

--- stringCalc.js
+++ stringCalc.js
@@ -3,10 +3,11 @@ function StringCalc() {
 
 StringCalc.prototype.Add = function (operand1, operand2) {
     var result;
+    var errResult = 'Err';
     var number1 = parseInt(operand1, 10);
-    if (isNaN(number1)) return 'Err';
+    if (isNaN(number1)) return errResult;
     var number2 = parseInt(operand2, 10);
-    if (isNaN(number2)) return 'Err';
+    if (isNaN(number2)) return errResult;
     result = number1 + number2;
     return result.toString();
 };
