3b3a97b58d153b5bcb7869aa8aac143e0195eeba
Teamzone <teamzone@aboutagile.com>
2017-08-07 11:54:31 +0000

    Negative numbers aren't handled and hence expected failure occured

--- stringCalcTests.js
+++ stringCalcTests.js
@@ -82,6 +82,11 @@ describe("String Calculator Tests", function () {
         done();
     });
     
+    it("'-1' + '1' returns '0' - checking addition of a negative number and a positive number", function (done) {
+        exerciseAndVerify('-1', '1', '0');
+        done();
+    });
+    
     it("It's a string calculator and therefore it needs to return a string result", function (done) {
         // 1. Setup
         
