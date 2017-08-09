4509e7d23e6fb39964011dc28788e80d070462e0
Teamzone <teamzone@aboutagile.com>
2017-08-09 00:24:34 +0000

    In the middle of a major refactoring to extract an object to help reduce duplication.  Tests are passing

--- stringCalc.js
+++ stringCalc.js
@@ -1,66 +1,72 @@
 function StringCalc() {
 }
 
+function OperationCharacteristics(operand1, operand2) {
+  this.operand1 = operand1;
+  this.operand2 = operand2;
+}
+
 StringCalc.prototype.Add = function (operand1, operand2) {
     var result = '';
     var errResult = 'Err';
-    var isSubtraction = false;
-    var lengthOperand1 = operand1.length;
-    var lengthOperand2 = operand2.length;
-    var isResultNegative = false;
-    var overflow = '';
+    var operationCharacteristics = new OperationCharacteristics(operand1, operand2);
+    operationCharacteristics.isSubtraction = false;
+    operationCharacteristics.lengthOperand1 = operand1.length;
+    operationCharacteristics.lengthOperand2 = operand2.length;
+    operationCharacteristics.isResultNegative = false;
+    operationCharacteristics.overflow = '';
     
-    if (operand1.charAt(0) === "-") {
+    if (operationCharacteristics.operand1.charAt(0) === "-") {
         operand1 = operand1.substr(1);
-        isSubtraction = !isSubtraction;
-        lengthOperand1--;
+        operationCharacteristics.isSubtraction = !operationCharacteristics.isSubtraction;
+        operationCharacteristics.lengthOperand1--;
     }
     
     if (operand2.charAt(0) === "-") {
         operand2 = operand2.substr(1);
-        isResultNegative = isSubtraction;
-        isSubtraction = !isSubtraction;
-        lengthOperand2--;
+        operationCharacteristics.isResultNegative = operationCharacteristics.isSubtraction;
+        operationCharacteristics.isSubtraction = !operationCharacteristics.isSubtraction;
+        operationCharacteristics.lengthOperand2--;
     }
     
-    var longestNumberLength = lengthOperand1;
-    if (lengthOperand2 > longestNumberLength)
-        longestNumberLength = lengthOperand2;
+    var longestNumberLength = operationCharacteristics.lengthOperand1;
+    if (operationCharacteristics.lengthOperand2 > longestNumberLength)
+        longestNumberLength = operationCharacteristics.lengthOperand2;
         
-    lengthOperand1--;
-    lengthOperand2--;        
+    operationCharacteristics.lengthOperand1--;
+    operationCharacteristics.lengthOperand2--;        
     for (var i = longestNumberLength - 1; i >= 0; i--) {
         var number1 = 0;
-        if (lengthOperand1 >= 0) {  
-            number1 = parseTheNextNumber(operand1, lengthOperand1, overflow);
-            overflow = '';            
+        if (operationCharacteristics.lengthOperand1 >= 0) {  
+            number1 = parseTheNextNumber(operand1, operationCharacteristics.lengthOperand1, operationCharacteristics.overflow);
+            operationCharacteristics.overflow = '';            
             if (isNaN(number1)) return errResult;                
         }
         
         var number2 = 0;
-        if (lengthOperand2 >= 0) {  
-            number2 = parseTheNextNumber(operand2, lengthOperand2, overflow);
-            overflow = ''; 
+        if (operationCharacteristics.lengthOperand2 >= 0) {  
+            number2 = parseTheNextNumber(operand2, operationCharacteristics.lengthOperand2, operationCharacteristics.overflow);
+            operationCharacteristics.overflow = ''; 
             if (isNaN(number2)) return errResult;                
         }   
         
         var intermediateResult = 0;
-        if (isSubtraction)
+        if (operationCharacteristics.isSubtraction)
             intermediateResult = number1 - number2;
         else
             intermediateResult = number1 + number2;
         if (intermediateResult > 9) {
             var overflowResult = intermediateResult.toString();
-            overflow = overflowResult.substring(0, 1);
+            operationCharacteristics.overflow = overflowResult.substring(0, 1);
             intermediateResult = parseInt(overflowResult.charAt(1), 10);
         }
-        lengthOperand1--;
-        lengthOperand2--;
+        operationCharacteristics.lengthOperand1--;
+        operationCharacteristics.lengthOperand2--;
         result = intermediateResult.toString() + result;
     }
-    if (overflow.length > 0)
-        result = overflow + result;
-    if (isResultNegative)
+    if (operationCharacteristics.overflow.length > 0)
+        result = operationCharacteristics.overflow + result;
+    if (operationCharacteristics.isResultNegative)
         result = '-' + result;
     var isAllZeroesCheck = result.replace(/0/gi, '');
     if (isAllZeroesCheck.length === 0)
